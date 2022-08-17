670e45b53f21cd552052b5121d12fa4314ed0207
Storm OBryant <vmarie1997@gmail.com>
2022-08-16 19:39:58 -0500

    deployed, but one image broken, updated gh link

--- lab03/src/components/Footer.js
+++ lab03/src/components/Footer.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Footer extends Component {
   render() {
     return (
-      <footer>Storm O'Bryant<br></br>&copy;StormyWeatherCreations, {this.props.year}<br></br>Check out my work on <a href='https://github.com/NunuMarie3000?tab=repositories' target={'_blank'} rel="noreferrer">Github</a>!</footer>
+      <footer>Storm O'Bryant<br></br>&copy;StormyWeatherCreations, {this.props.year}<br></br>Check out my work on <a href='https://github.com/NunuMarie3000/' target={'_blank'} rel="noreferrer">Github</a>!</footer>
     )
   }
 }
